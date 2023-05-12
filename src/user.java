a2241274e543bdee8bd9cf7f64c6bdd491737376
Standort <marinc.lovro@gmail.com>
2023-05-13 01:40:34 +0200

    User class

---
+++ src/user.java
@@ -0,0 +1,77 @@
+import java.time.LocalDate;
+
+public class user {
+    private int userID;
+    private String name;
+    private LocalDate dateOfBirth;
+    private float height;
+    private float weight;
+    private String gender;
+    
+    public user(int userID, String name, LocalDate dateOfBirth, float height, float weight, String gender) {
+        this.userID = userID;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+    }
+    
+    public user(String name, LocalDate dateOfBirth, float height, float weight, String gender) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+    }
+    
+    // Getters and setters
+    
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
