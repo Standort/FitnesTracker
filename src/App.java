89adba6726a0447bf3cb1c381db16a5b891be454
Standort <marinc.lovro@gmail.com>
2023-05-13 01:44:30 +0200

    User class test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        LocalDate dateOfBirth = LocalDate.of(1990, 5, 13);
+        user user = new user("John Doe", dateOfBirth, 180.0f, 75.0f, "Male");
         
+        // Accessing and printing user information
+        System.out.println("User ID: " + user.getUserID());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Date of Birth: " + user.getDateOfBirth());
+        System.out.println("Height: " + user.getHeight());
+        System.out.println("Weight: " + user.getWeight());
+        System.out.println("Gender: " + user.getGender());
     }
 }
