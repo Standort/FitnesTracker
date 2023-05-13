01594afb03ff51507186a701f1311be5009a5e0c
Standort <marinc.lovro@gmail.com>
2023-05-14 00:00:08 +0200

    Added height, weight and dateofbirth field + check

--- src/CreateUserGUI.java
+++ src/CreateUserGUI.java
@@ -1,8 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import javax.swing.*;
 
 public class CreateUserGUI {
     private JFrame frame;
     private JTextField nameField;
+    private JTextField heightField;
+    private JTextField weightField;
+    private JTextField dateOfBirth;
     private JButton createButton;
     private JPanel panel;
 
@@ -14,10 +21,21 @@ public class CreateUserGUI {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         // Create a JTextField for entering the name
-        nameField = new JTextField(20);
+        nameField = new JTextField(0);
         panel.add(new JLabel("Name: "));
         panel.add(nameField);
 
+        heightField = new JTextField(0);
+        panel.add(new JLabel("Height: "));
+        panel.add(heightField);
+
+        weightField = new JTextField(0);
+        panel.add(new JLabel("Weight: "));
+        panel.add(weightField);
+
+        dateOfBirth = new JTextField(0);
+        panel.add(new JLabel("Date of birth: "));
+        panel.add(dateOfBirth);
         // Create a JButton for creating the user
         createButton = new JButton("Create");
         createButton.addActionListener(e -> createUser());
@@ -37,17 +55,68 @@ public class CreateUserGUI {
         SwingUtilities.invokeLater(CreateUserGUI::new);
     }
 
-    private void createUser() {
-        // Get the entered name from the text field
+    public boolean checkData() {
         String name = nameField.getText();
+        String heightText = heightField.getText();
+        String weightText = weightField.getText();
+        String dateOfBirthText = dateOfBirth.getText();
 
-        // Perform further operations to create the user
-        // ...
+        // Check if name is not empty
+        if (name.isEmpty()) {
+            JOptionPane.showMessageDialog(frame, "Please enter a name.");
+            return false;
+        }
 
-        // Display a message or perform any other actions
-        JOptionPane.showMessageDialog(frame, "User created: " + name);
+        // Check if height is a valid float value
+        try {
+            float height = Float.parseFloat(heightText);
+            if (height <= 0) {
+                JOptionPane.showMessageDialog(frame, "Please enter a valid height greater than 0.");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(frame, "Please enter a valid numeric height value.");
+            return false;
+        }
 
-        // Reset the text field
-        nameField.setText("");
+        // Check if weight is a valid float value
+        try {
+            float weight = Float.parseFloat(weightText);
+            if (weight <= 0) {
+                JOptionPane.showMessageDialog(frame, "Please enter a valid weight greater than 0.");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(frame, "Please enter a valid numeric weight value.");
+            return false;
+        }
+
+        // Check if dateOfBirth is in a valid format
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate.parse(dateOfBirthText, formatter);
+        } catch (DateTimeParseException e) {
+            JOptionPane.showMessageDialog(frame, "Please enter a valid date of birth in the format yyyy-MM-dd.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void createUser() {
+        if (checkData()) {
+            // Get the entered name from the text field
+            String name = nameField.getText();
+
+            // Perform further operations to create the user
+            // ...
+
+            // Display a message or perform any other actions
+            JOptionPane.showMessageDialog(frame, "User created: " + name);
+
+            // Reset the text field
+            nameField.setText("");
+        }
+
     }
 }
