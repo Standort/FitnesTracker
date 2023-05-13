970841eda59236106150d4a02c35845569f7677e
Standort <marinc.lovro@gmail.com>
2023-05-13 02:15:44 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         LocalDate dateOfBirth = LocalDate.of(1990, 5, 13);
         user user = new user("John Doe", dateOfBirth, 180.0f, 75.0f, "Male");
-        Workout workout = new Workout(0, "Cycling", 15);
+        Workout workout = new Workout(0, "Cycling", 15.0f);
         System.out.println(workout.getCaloriesPerMinute());
         // Accessing and printing user information
         System.out.println("User ID: " + user.getUserID());
