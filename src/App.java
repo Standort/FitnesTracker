58d1037af912134a510cf2f7073943a62f33a2b9
Standort <marinc.lovro@gmail.com>
2023-05-13 02:25:21 +0200

    Added session class

--- src/App.java
+++ src/App.java
@@ -5,13 +5,8 @@ public class App {
         LocalDate dateOfBirth = LocalDate.of(1990, 5, 13);
         User user = new User("John Doe", dateOfBirth, 180.0f, 75.0f, "Male");
         Workout workout = new Workout(0, "Cycling", 15.0f);
-        System.out.println(workout.getCaloriesPerMinute());
-        // Accessing and printing user information
-        System.out.println("User ID: " + user.getUserID());
-        System.out.println("Name: " + user.getName());
-        System.out.println("Date of Birth: " + user.getDateOfBirth());
-        System.out.println("Height: " + user.getHeight());
-        System.out.println("Weight: " + user.getWeight());
-        System.out.println("Gender: " + user.getGender());
+        Session session = new Session(user, workout, 2);
+
+        System.out.println(session.getCaloriesBurned());
     }
 }
--- src/Session.java
+++ src/Session.java
@@ -1,10 +1,16 @@
+import java.time.LocalDateTime;
+
 public class Session {
     private User user;
     private Workout workout;
+    private LocalDateTime dateOfCreation;
+    private int duration;//seconds
     
-    public Session(User user, Workout workout) {
+    public Session(User user, Workout workout, int duration) {
         this.user = user;
         this.workout = workout;
+        this.dateOfCreation = LocalDateTime.now();
+        this.duration = duration;
     }
     
     public User getUser() {
@@ -21,5 +27,26 @@ public class Session {
     
     public void setWorkout(Workout workout) {
         this.workout = workout;
+    }
+    
+    public LocalDateTime getDateOfCreation() {
+        return dateOfCreation;
+    }
+    
+    public void setDateOfCreation(LocalDateTime dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public float getCaloriesBurned(){
+        float cb = workout.getCaloriesPerMinute() * this.duration;
+        return cb;
+
     }
 }
